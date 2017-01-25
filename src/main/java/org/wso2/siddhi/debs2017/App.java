321036463f1153b3e7af2e2b732ec889e8121785
TEMP <temp@Admins-MacBook-Pro.local>
2017-01-25 12:00:07 +0530

    DEBS2017

---
+++ src/main/java/org/wso2/siddhi/debs2017/App.java
@@ -0,0 +1,13 @@
+package org.wso2.siddhi.debs2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
